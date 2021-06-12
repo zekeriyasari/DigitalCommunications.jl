7ac2e6afe42aa996d94d211c1ea590ac3c0beb15
zekeriya.sari <zekeriya.sari@deu.edu.tr>
2021-06-12 15:21:25 +0300

    Updated compats.

--- example/vector_simulations/montecarloask.jl
+++ example/vector_simulations/montecarloask.jl
@@ -5,29 +5,33 @@ using DigitalCommunications
 using Plots 
 
 # Simulation parameters 
-k = 3
-M = 2^k 
-nsymbols = Int(1e6) 
-nbits = k * nsymbols
-ebno = collect(0 : 10)    
-esno = ebno .+ 10 * log10(k)
+theme(:default)
+plt = plot(legend=:bottomleft) 
+for k in 1 : 3
+    M = 2^k 
+    nsymbols = Int(1e6) 
+    nbits = k * nsymbols
+    ebno = collect(0 : 10)    
+    esno = ebno .+ 10 * log10(k)
 
-# Communcation system components  
-gen = BitGenerator(nsymbols) 
-modulator = Modulator(ASK(M))
-channel = AWGNChannel() 
-detector = Detector(alphabet(modulator))
+    # Communcation system components  
+    gen = SymbolGenerator(nsymbols, M) 
+    modulator = Modulator(ASK(M))
+    channel = AWGNChannel() 
+    detector = Detector(modulator(1:M))
 
-# Monte Carlo simulation 
-message = coding(gen.bits)
-symerr = zeros(length(esno))
-for i in 1 : length(symerr)
-    channel.esno = esno[i]  # Update channel snr
-    mbar = gen.bits |> coding |> modulator |> channel |> detector  # Extracted message signal 
-    symerr[i] = sum(mbar .!= message) / length(message)  # Symbol error rate 
+    # Monte Carlo simulation 
+    message = gen.symbols
+    symerr = zeros(length(esno))
+    for i in 1 : length(symerr)
+        channel.esno = esno[i]  # Update channel snr
+        mbar = message |> modulator |> channel |> detector  # Extracted message signal 
+        symerr[i] = sum(mbar .!= message) / length(message)  # Symbol error rate 
+    end
+
+    plot!(ebno, berask.(ebno, M), yscale=:log10, lw=0.5, 
+        markershape=:auto, color=:black, gridalpha=0.9, minorgrid=true, minorgridalpha=0.5, label="$M-ASK-theoretical")
+    plot!(ebno, symerr,  yscale=:log10, lw=0.5, 
+        markershape=:auto, color=:black, gridalpha=0.9, minorgrid=true, minorgridalpha=0.5, label="$M-ASK-montecarlo")
 end
-
-# Plots
-plt = plot(title="$M-ASK", xlabel="ebno [dB]", ylabel="Pe") 
-plot!(ebno, berask.(ebno, M), marker=:circle, yscale=:log10, label="theoretical")
-plot!(ebno, symerr, marker=:circle, yscale=:log10, label="montecarlo")
+display(plt) 
